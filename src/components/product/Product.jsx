0094e61b5a7339776b2546dfdcc2e6c07cb9f677
Brian Michel <bmoralescutting@gmail.com>
2025-08-02 18:36:24 -0400

    Update product dark mode

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,10 +1,13 @@
 import "./product.css";
+import { useContext } from "react";
+import { ThemeContext } from "../../ThemeContext";
 
 const Product = ({ img, link }) => {
-  
+   const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
 
   return (
-    <div className="p">
+    <div className={`p ${darkMode ? "p-dark" : "p-light"}`}>
       <a href={link} target="_blank" rel="noreferrer">
         <img src={img} alt="" className="p-img" />
       </a>
